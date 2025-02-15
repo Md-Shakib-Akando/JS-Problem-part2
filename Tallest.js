b01f13b80345fa74a6a79ad135f645928be50905
Md-Shakib-Akando <shakibakando2@gmail.com>
2025-02-15 18:45:01 +0600

    tallest height

---
+++ Tallest.js
@@ -0,0 +1,16 @@
+const height=[65, 66, 68, 72, 78, 60 ,65,66];
+
+function getMax(numbers){
+    let max= numbers[0];
+    for(const num of numbers){
+        if(num>max){
+            max=num;
+        }
+    }
+
+    return max;
+}
+
+const max = getMax(height);
+
+console.log('the max height is', max);
