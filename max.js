1c8503767701780079263e209264e7e007e7ae07
Md-Shakib-Akando <shakibakando2@gmail.com>
2025-02-15 18:38:05 +0600

    Javascript Problem

---
+++ max.js
@@ -0,0 +1,18 @@
+const jim=89;
+const kim=45;
+const tim=23;
+
+if(jim>kim && jim> tim){
+    console.log("Jim is the large");
+
+}
+
+else if(kim>jim && kim>tim)
+{
+    console.log('kim is the large');
+
+}
+
+else{
+    console.log("tim is best");
+}
